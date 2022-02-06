2eaee649589b42ac8b00c477a36c88cfebf5cdab
scottrules44 <scottrules44@gmail.com>
2022-02-06 18:37:53 -0500

    initial commit

---
+++ plugins/2018.3326/iphone-sim/metadata.lua
@@ -0,0 +1,12 @@
+local metadata =
+{
+	plugin =
+	{
+		format = 'staticLibrary',
+		staticLibs = {  },
+		frameworks = { 'GoogleAppMeasurement', 'GoogleAppMeasurementIdentitySupport', 'GoogleMobileAds', 'FBLPromises', 'GoogleUtilities', 'nanopb', "UserMessagingPlatform", "AppLovinMediationGoogleAdapter" },
+		frameworksOptional = {},
+	},
+}
+
+return metadata
